c65ec73863e86db525e3d85a825e734d0b10a44e
nimeshkasun <nimesh.ekanayaka7@gmail.com>
2021-06-22 08:10:12 +0530

    Registration form error

--- registration/index.php
+++ registration/index.php
@@ -1,26 +1,32 @@
 <!doctype html>
 <html lang="en">
 <?php
+  ob_start();
+  session_start();
   if(isset($_GET['lr'])){
-    session_start();
     $_SESSION['check'] = "lr";
   }else if(isset($_GET['fr'])){
-    session_start();
     $_SESSION['check'] = "fr";
-  }else{
-    session_start();
   }
 
   if(isset($_SESSION['check'])){
     if($_SESSION['check'] == "lr"){
       header('location: https://forms.office.com/Pages/ResponsePage.aspx?id=IVnGy3moa0KZFuBhG2D75TfVxligsdRKttmwZwrSIBNUOTVWTFRJU0I2QjFOVUdITzAxNE1DV0Y5Ui4u');
+      ob_end_flush();
+      die();
     }else if($_SESSION['check'] == "fr"){
       header('location: https://forms.office.com/Pages/ResponsePage.aspx?id=IVnGy3moa0KZFuBhG2D75TfVxligsdRKttmwZwrSIBNUOTVWTFRJU0I2QjFOVUdITzAxNE1DV0Y5Ui4u');
+      ob_end_flush();
+      die();
     }else{
       header('location: ../');
+      ob_end_flush();
+      die();
     }
   }else{
     header('location: ../');
+    ob_end_flush();
+      die();
   }
 ?>
 </head>
